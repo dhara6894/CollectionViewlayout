c54474cabcc5fb86a889864508b5e84ac886383f
Dhara Patel <dharapatel@Dharas-Mac-mini.local>
2018-07-29 22:44:29 +0530

    Initial commit

---
+++ CollectionViewLayout/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViewLayout
+//
+//  Created by Dhara Patel on 29/07/18.
+//  Copyright © 2018 Dhara Patel. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ CollectionViewLayout/CollectionViewLayout.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewLayout.swift
+//  CollectionViewLayout
+//
+//  Created by Dhara Patel on 29/07/18.
+//  Copyright © 2018 Dhara Patel. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewLayout: UICollectionViewLayout {
+
+}
